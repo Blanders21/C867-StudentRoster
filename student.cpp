11c0354260ea3a46b7e3363feb0d1dbdadb35762
Braeten <bander37@wgu.edu>
2026-04-16 22:20:17 -0500

    Added getters, setters, and print function to student class.

---
+++ student.cpp
@@ -0,0 +1,54 @@
+#include "student.h"
+#include <iostream>
+using namespace std;
+
+//Constructor
+Student::Student(string id, string first, string last, string email, int age, int d1, int d2, int d3, DegreeProgram dp) {
+    studentID = id;
+    firstName = first;
+    lastName = last;
+    this->email = email;
+    this->age = age;
+    daysInCourse[0] = d1;
+    daysInCourse[1] = d2;
+    daysInCourse[2] = d3;
+    degreeProgram = dp;
+}
+
+// Getters
+string Student::getStudentID() { return studentID; }
+string Student::getFirstName() { return firstName; }
+string Student::getLastName() { return lastName; }
+string Student::getEmail() { return email; }
+int Student::getAge() { return age; }
+int* Student::getDaysInCourse() { return daysInCourse; }
+DegreeProgram Student::getDegreeProgram() { return degreeProgram; }
+
+// Setters
+void Student::setStudentID(string id) { studentID = id; }
+void Student::setFirstName(string first) { firstName = first; }
+void Student::setLastName(string last) { lastName = last; }
+void Student::setEmail(string email) { this->email = email; }
+void Student::setAge(int age) { this->age = age; }
+
+void Student::setDaysInCourse(int d1, int d2, int d3) {
+    daysInCourse[0] = d1;
+    daysInCourse[1] = d2;
+    daysInCourse[2] = d3;
+}
+
+void Student::setDegreeProgram(DegreeProgram dp) { degreeProgram = dp; }
+
+
+// Print function
+void Student::print() {
+    cout << studentID << "\t"
+        << "First Name: " << firstName << "\t"
+        << "Last Name: " << lastName << "\t"
+        << "Email: " << email << "\t"
+        << "Age: " << age << "\t"
+        << "Days: {" << daysInCourse[0] << ", "
+        << daysInCourse[1] << ", "
+        << daysInCourse[2] << "} "
+        << "Degree: " << degreeProgram << endl;
+}
--- student.h
+++ student.h
@@ -1,1 +1,40 @@
 #pragma once
+#include <string>
+#include "degree.h"
+using namespace std;
+
+class Student {
+private:
+    string studentID;
+    string firstName;
+    string lastName;
+    string email;
+    int age;
+    int daysInCourse[3];
+    DegreeProgram degreeProgram;
+
+public:
+    // Constructor
+    Student(string id, string first, string last, string email, int age, int d1, int d2, int d3, DegreeProgram dp);
+
+    // Getters
+    string getStudentID();
+    string getFirstName();
+    string getLastName();
+    string getEmail();
+    int getAge();
+    int* getDaysInCourse();
+    DegreeProgram getDegreeProgram();
+
+    // Setters
+    void setStudentID(string id);
+    void setFirstName(string first);
+    void setLastName(string last);
+    void setEmail(string email);
+    void setAge(int age);
+    void setDaysInCourse(int d1, int d2, int d3);
+    void setDegreeProgram(DegreeProgram dp);
+
+    void print();
+};
+
